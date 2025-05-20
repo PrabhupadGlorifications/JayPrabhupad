f3588935fc8bab026289e06f224cf6595dbbd9fe
PrabhupadGlorifications <jayshrilaprabhupad108@gmail.com>
2025-05-20 17:59:31 +0530

    i added scroll effect

---
+++ scroll.js
@@ -0,0 +1,9 @@
+
+  ScrollReveal().reveal('.hero-section', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true, // <--- This makes it animate again on re-entry
+    delay: 100
+  });
