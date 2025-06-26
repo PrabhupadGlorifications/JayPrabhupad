5dbf130afa1bf14e674d9a07c944c1f2dfcc7453
PrabhupadGlorifications <jayshrilaprabhupad108@gmail.com>
2025-06-26 11:58:15 +0530

    chamge17

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCWxaABz6OSVfSniGEMdjbrWJDgmNRlzNQ",
-  authDomain: "pgsite1957.firebaseapp.com",
-  projectId: "pgsite1957",
-  storageBucket: "pgsite1957.firebasestorage.app",
-  messagingSenderId: "464761905973",
-  appId: "1:464761905973:web:c0d182ff433b029f8be27c",
-  databaseURL: "https://pgsite1957-default-rtdb.firebaseio.com" // ⚠️ Add this line manually
-};
--- script.js
+++ script.js
@@ -48,30 +48,17 @@ function addSearchFunctionality(searchInputId, listId) {
 addSearchFunctionality('writingsSearch', 'writingsList');
 addSearchFunctionality('pastimesSearch', 'pastimesList');
 
-function handleLike(btn) {
-  let countSpan = btn.querySelector('.likeCount');
-  let count = parseInt(countSpan.innerText);
-  count++;
-  countSpan.innerText = count;
-}
-
-function handleShare() {
-  if (navigator.share) {
-    navigator.share({
-      title: document.title,
-      text: 'Check out this article!',
-      url: window.location.href
-    }).then(() => {
-      console.log('Shared successfully');
-    }).catch(err => {
-      console.error('Error sharing:', err);
-    });
-  } else {
-    alert("This browser doesn't support sharing.");
-  }
-}
 
 // Like button toggle
+const firebaseConfig = {
+  apiKey: "AIzaSyCWxaABz6OSVfSniGEMdjbrWJDgmNRlzNQ",
+  authDomain: "pgsite1957.firebaseapp.com",
+  projectId: "pgsite1957",
+  storageBucket: "pgsite1957.firebasestorage.app",
+  messagingSenderId: "464761905973",
+  appId: "1:464761905973:web:c0d182ff433b029f8be27c",
+  databaseURL: "https://pgsite1957-default-rtdb.firebaseio.com" // ⚠️ Add this line manually
+};
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
