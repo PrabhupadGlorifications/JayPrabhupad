1128c181b5acf42c555bfe2efdf2fd1e4a9dd148
PrabhupadGlorifications <jayshrilaprabhupad108@gmail.com>
2025-06-26 12:01:41 +0530

    wde

--- script.js
+++ script.js
@@ -53,11 +53,11 @@ addSearchFunctionality('pastimesSearch', 'pastimesList');
 const firebaseConfig = {
   apiKey: "AIzaSyCWxaABz6OSVfSniGEMdjbrWJDgmNRlzNQ",
   authDomain: "pgsite1957.firebaseapp.com",
+  databaseURL: "https://pgsite1957-default-rtdb.firebaseio.com",
   projectId: "pgsite1957",
   storageBucket: "pgsite1957.firebasestorage.app",
   messagingSenderId: "464761905973",
-  appId: "1:464761905973:web:c0d182ff433b029f8be27c",
-  databaseURL: "https://pgsite1957-default-rtdb.firebaseio.com" // ⚠️ Add this line manually
+  appId: "1:464761905973:web:c0d182ff433b029f8be27c"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
